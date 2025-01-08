486fc2e119cba073ca6ad36ba6c63915d1a85718
Eric Jessee <ericjessee@gmail.com>
2025-01-08 00:40:18 -0700

    addded build/upload flow script (very not flexible)

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 
 filename=sys.argv[1]
 # Configure the serial port
-port_name = "/dev/ttyUSB0"  # Replace with your port name, e.g., "COM3" on Windows or "/dev/ttyUSB0" on Linux
-baud_rate = 9600    # Adjust as needed
+port_name = "/dev/ttyUSB0"
+baud_rate = 9600
 ser = init_ser(port_name, baud_rate)
 get_bsl_ver(ser)
 clear_flash(ser)
@@ -17,6 +17,6 @@ for seg in segments:
     data = read_bin(f'elf_data/{seg['filename']}')
     write_data(ser, addr, data)
 
-#reset device to begin code execution
+#reset device to begin code execution (provided reset vector is defined)
 reset_msp(ser)
 print("done. exiting...")
